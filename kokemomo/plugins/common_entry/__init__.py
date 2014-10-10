1a265b4d155ee42e3f0b4d3061baf537fd531de6
hiroki8080 <hiroki-m@gaia.eonet.ne.jp>
2014-10-10 12:32:59 +0900

    各モデルにてget_sessionを行っていたものを、db_managerを介して行うように集約 各プラグインの__init__の処理をコントローラに移動

--- kokemomo/plugins/common_entry/__init__.py
+++ kokemomo/plugins/common_entry/__init__.py
@@ -1,37 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from kokemomo.lib.bottle import template, route, static_file, url, request, response, redirect
-from kokemomo.plugins.common_entry.controller.km_entry import get_columns, get_title, entry
+import kokemomo.plugins.common_entry.controller.km_entry
 
 __author__ = 'hiroki'
-
-@route('/common_entry/js/<filename>', name='common_entry_static_js')
-def common_entry_js_static(filename):
-    """
-    set javascript files.
-    :param filename: javascript file name.
-    :return: static path.
-    """
-    return static_file(filename, root='kokemomo/plugins/common_entry/view/resource/js')
-
-
-@route('/common_entry/css/<filename>', name='common_entry_static_css')
-def common_entry_css_static(filename):
-    """
-    set css files.
-    :param filename: css file name.
-    :return: static path.
-    """
-    return static_file(filename, root='kokemomo/plugins/common_entry/view/resource/css')
-
-
-@route('/common_entry')
-def common_entry():
-    columns = get_columns()
-    title = get_title()
-    return template('kokemomo/plugins/common_entry/view/entry', url=url, title=title, columns=columns) # TODO: パス解決を修正する
-
-@route('/common_entry/save', method='POST')
-def common_entry_save():
-    entry(request)
---
+++ kokemomo/plugins/engine/controller/km_db_manager.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from kokemomo.plugins.engine.utils.config import get_database_setting
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+__author__ = 'hiroki'
+
+Base = declarative_base()
+
+class KMDBManager():
+
+    def __init__(self, name):
+        self.initialize_session(name)
+
+    def initialize_session(self, name):
+        sql_url = get_database_setting(name)
+        engine = create_engine(sql_url, encoding='utf-8', echo=True)
+        self.Session = sessionmaker(bind=engine)()
+        Base.metadata.create_all(engine)
+
+    def get_session(self):
+        return self.Session
