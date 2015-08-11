b2f047580a1a964cbe7c982b36b79d9a1df6947f
hiroki8080 <hiroki-m@gaia.eonet.ne.jp>
2015-08-11 16:16:29 +0900

    Storage Adapterを設定ファイルから読み込めるように
    - SQLAlchemyAdapterも別ファイルに分離

--- kokemomo/plugins/engine/controller/km_storage/adapter/sqlalchemy_adapter.py
+++ kokemomo/plugins/engine/controller/km_storage/adapter/sqlalchemy_adapter.py
@@ -1,57 +1,12 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, func
+from sqlalchemy import (
+    Column, Integer, String, Text, DateTime, Boolean, func
+)
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from abc import ABCMeta, abstractmethod
-
-default_adapter = None
-
-
-class Storage(object):
-    def __init__(self, adapter=None):
-        if adapter is None:
-            self.adapter = default_adapter
-        else:
-            self.adapter = adapter
-
-        if self.adapter.fields:
-            for field in self.adapter.fields:
-                setattr(self, field.__name__, field)
-
-    def init(self, *args, **kwargs):
-        self.adapter.init(*args, **kwargs)
-
-    def set(self, *args, **kwargs):
-        self.adapter.set(*args, **kwargs)
-
-    def get(self, *args, **kwargs):
-        return self.adapter.get(*args, **kwargs)
-
-    @property
-    def Model(self):
-        return self.adapter.Model
-
-
-class BasicAdapter(object):
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def init(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
-    def set(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
-    def get(self, *args, **kwargs):
-        pass
-
-
-class RdbAdapter(object):
-    __metaclass__ = ABCMeta
+from kokemomo.plugins.engine.controller.km_storage import *
 
 
 class BaseModel(object):
@@ -122,13 +77,3 @@ class SQLAlchemyAdapter(BasicAdapter, RdbAdapter):
 
     def get(self, *args, **kwargs):
         pass
-
-from ..utils.km_config import get_database_setting
-
-
-sql_url = get_database_setting('engine')
-db = Storage(
-    adapter=SQLAlchemyAdapter(sql_url)
-)
-# file = Storage(adapter=FileAdapter('~/hoge.md')
-# redis = Storage(adapter=RedisAdapter())
