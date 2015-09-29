eb8011e9e1ae86ac74e8d6046a2cbb978272a920
hiroki8080 <hiroki-m@gaia.eonet.ne.jp>
2015-09-29 21:46:50 +0900

    モデルベースに移行予定の処理を削除、コメントアウト

--- kokemomo/plugins/blog/model/km_blog_category.py
+++ kokemomo/plugins/blog/model/km_blog_category.py
@@ -39,62 +39,62 @@ def create(id, session):
     return category
 
 
-def find(id, session):
-    result = None
-    for category in session.query(KMBlogCategory).filter_by(id=id).all():
-        result = category
-    return result
-
-
-def find_by_info(info_id, session):
-    result = []
-    for info in session.query(KMBlogCategory).filter_by(info_id=info_id).all():
-        result.append(info)
-    return result
-
-
-def find_all(session):
-    result = []
-    fetch = session.query(KMBlogCategory)
-    for category in fetch.all():
-        result.append(category)
-    return result
-
-
-def add(category, session):
-    try:
-        session.add(category)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
-
-
-def update(category, session):
-    try:
-        session.merge(category)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
-
-
-def delete(id, session):
-    fetch_object = session.query(KMBlogCategory).filter_by(id=id).one()
-    try:
-        session.delete(fetch_object)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
-
-
-def delete_by_info(info_id, session):
-    fetch = session.query(KMBlogCategory).filter_by(info_id=info_id).all()
-    try:
-        for target in fetch:
-            session.delete(target)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
+# def find(id, session):
+#     result = None
+#     for category in session.query(KMBlogCategory).filter_by(id=id).all():
+#         result = category
+#     return result
+#
+#
+# def find_by_info(info_id, session):
+#     result = []
+#     for info in session.query(KMBlogCategory).filter_by(info_id=info_id).all():
+#         result.append(info)
+#     return result
+#
+#
+# def find_all(session):
+#     result = []
+#     fetch = session.query(KMBlogCategory)
+#     for category in fetch.all():
+#         result.append(category)
+#     return result
+#
+#
+# def add(category, session):
+#     try:
+#         session.add(category)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
+#
+#
+# def update(category, session):
+#     try:
+#         session.merge(category)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
+#
+#
+# def delete(id, session):
+#     fetch_object = session.query(KMBlogCategory).filter_by(id=id).one()
+#     try:
+#         session.delete(fetch_object)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
+#
+#
+# def delete_by_info(info_id, session):
+#     fetch = session.query(KMBlogCategory).filter_by(info_id=info_id).all()
+#     try:
+#         for target in fetch:
+#             session.delete(target)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
--- kokemomo/plugins/blog/model/km_blog_comment.py
+++ kokemomo/plugins/blog/model/km_blog_comment.py
@@ -2,7 +2,6 @@
 # -*- coding:utf-8 -*-
 from kokemomo.plugins.engine.utils.km_model_utils import *
 from kokemomo.plugins.engine.controller.km_storage import storage
-from sqlalchemy.types import Text
 
 __author__ = 'hiroki'
 
@@ -31,52 +30,52 @@ class KMBlogComment(storage.Model):
         return create_json(self)
 
 
-def find(id, session):
-    result = None
-    for comment in session.query(KMBlogComment).filter_by(id=id).all():
-        result = comment
-    return result
+# def find(id, session):
+#     result = None
+#     for comment in session.query(KMBlogComment).filter_by(id=id).all():
+#         result = comment
+#     return result
 
-
-def find_by_article_id(article_id, session):
-    result = []
-    fetch = session.query(KMBlogComment).filter_by(article_id=article_id).all()
-    for comment in fetch:
-        result.append(comment)
-    return result
-
-
-def find_all(session):
-    result = []
-    fetch = session.query(KMBlogComment)
-    for comment in fetch.all():
-        result.append(comment)
-    return result
-
-
-def add(comment, session):
-    try:
-        session.add(comment)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
-
-
-def update(comment, session):
-    try:
-        session.merge(comment)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
-
-
-def delete(id, session):
-    fetch_object = session.query(KMBlogComment).filter_by(id=id).one()
-    try:
-        session.delete(fetch_object)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise e
+#
+# def find_by_article_id(article_id, session):
+#     result = []
+#     fetch = session.query(KMBlogComment).filter_by(article_id=article_id).all()
+#     for comment in fetch:
+#         result.append(comment)
+#     return result
+#
+#
+# def find_all(session):
+#     result = []
+#     fetch = session.query(KMBlogComment)
+#     for comment in fetch.all():
+#         result.append(comment)
+#     return result
+#
+#
+# def add(comment, session):
+#     try:
+#         session.add(comment)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
+#
+#
+# def update(comment, session):
+#     try:
+#         session.merge(comment)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
+#
+#
+# def delete(id, session):
+#     fetch_object = session.query(KMBlogComment).filter_by(id=id).one()
+#     try:
+#         session.delete(fetch_object)
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise e
